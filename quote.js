946b2dc0a8ab0437da11e694114bd37a0f3e9a67
Cody Reandeau <codyreandeau@yahoo.com>
2016-11-18 13:36:37 -0500

    Added function that allows user to tweet the quote. Made minor css changes.

--- quote.js
+++ quote.js
@@ -4,7 +4,7 @@ var color =
   var rand = Math.floor(Math.random() * color.length);
   console.log(color[rand]);
     document.getElementsByTagName("body")[0].style.backgroundColor=color[rand];
-  document.getElementsByTagName("button")[0].style.backgroundColor=color[rand];
+  document.getElementsByClassName("btn")[0].style.backgroundColor=color[rand];
 };
 
 changeBGColor();
@@ -18,24 +18,25 @@ $(function() {
     event.preventDefault();
     changeBGColor();
     getQuote(author, text);
-	
   })
 });
+
+var tweet = "";
+$('#tweet').click(function() {
+    $(this).attr("href", "https://twitter.com/intent/tweet?text=" + tweet);
+})
 
 function getQuote(author, text) {
 
   var forismaticURL = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
 
   $.getJSON(forismaticURL, function(data) {
-
-    text.html(data.quoteText);
+      text.html(data.quoteText);
     if (data.quoteAuthor) {
       author.html(data.quoteAuthor);
-      author.attr("href", data.quoteLink);
     } else {
-      author.removeAttr("href");
       author.html("Anonymous");
     }
-    tweetThis = data.quoteText + " - " + data.quoteAuthor;
+    tweet = '"' + data.quoteText + '"' + " - " + data.quoteAuthor;
   });
 }
